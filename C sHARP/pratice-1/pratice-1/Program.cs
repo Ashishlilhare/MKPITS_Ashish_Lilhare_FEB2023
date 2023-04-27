fb8f1b645f57317870ef100a31cfeb8e4da5eee6
Ashishlilhare <124874469+Ashishlilhare@users.noreply.github.com>
2023-04-27 15:04:57 +0530

    uploded

---
+++ C sHARP/pratice-1/pratice-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratice_1
+{
+    internal class Program
+    {
+        //C# String Example
+
+        static void Main(string[] args)
+        {
+            string s1 = "hello";
+
+            char[] ch = { 'c', 's', 'h', 'a', 'r', 'p' };
+            string s2 = new string(ch);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.ReadLine();
+
+        }
+    }
+}
