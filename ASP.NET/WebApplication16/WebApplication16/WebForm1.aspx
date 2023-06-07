b51bc4fabee933775cd217ce70742fbb6daf1cec
Ashishlilhare <124874469+Ashishlilhare@users.noreply.github.com>
2023-06-07 17:53:30 +0530

    added

---
+++ ASP.NET/WebApplication16/WebApplication16/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication16.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Cookie<br />
+            <br />
+            <br />
+            Enter username<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="store in temp cookie" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Show temp Cookies" />
+            <br />
+            <br />
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ ASP.NET/WebApplication16/WebApplication16/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication16
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie1 = new HttpCookie("cook1");
+            cookie1.Values.Add("username",TextBox1.Text);
+            Response.Cookies.Add(cookie1);
+            Label1.Text = "Temp cookie added to client machine";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie1 = Request.Cookies["cook1"];
+            if (cookie1 != null)
+            {
+                string name = cookie1.Values["username"].ToString();
+                Label1.Text = "username :" + name;
+            }
+            else
+            {
+                Label1.Text = "no cookies exist";
+            }
+        }
+    }
+}
