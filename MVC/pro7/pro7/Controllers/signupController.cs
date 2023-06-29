bba9d612231cf0198136ff5d2830bccf9ca3380c
Ashishlilhare <124874469+Ashishlilhare@users.noreply.github.com>
2023-06-29 14:41:23 +0530

    added

---
+++ MVC/pro7/pro7/Controllers/signupController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pro7.Models;
+
+namespace pro7.Controllers
+{
+    public class signupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index(signup s) 
+        {
+            ViewBag.username = s.username;
+            
+
+            return View();
+
+        }
+    }
+}
---
+++ MVC/pro7/pro7/Models/signup.cs
@@ -0,0 +1,17 @@
+﻿namespace pro7.Models
+{
+    public class signup
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string gender { get; set; }
+        public bool subject1 { get; set; }
+        public bool subject2 { get; set; }
+        public city getcity { get; set; }
+
+    }
+    public enum city
+    {
+        nagpur,mumbai,pune,mangloore,keral,hyderabad
+    }
+}
